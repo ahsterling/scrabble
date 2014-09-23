77ba17585945429e46fd56846769506477349f81
Allie Sterling <sterlina89@gmail.com>
2014-09-23 11:40:19 -0700

    added check_exceptions class

--- scrabble.rb
+++ scrabble.rb
@@ -1,27 +1,19 @@
 class Scrabble
 
   def self.score(word)
-    score = 0
-    word = word.downcase
-    word.chars.each { |letter| score += letter_points(letter) }
-    return score
+    word_score = 0
+    unless word.downcase[/\d/]
+      word.downcase.chars.each { |letter| word_score += letter_points(letter) }
+    end
+    word_score
   end
 
   def self.highest_score_from(array_of_words)
     sorted_by_score = array_of_words.sort_by { |word| score(word) }
     max_score = score(sorted_by_score[-1])
     top_words = sorted_by_score.select { |word| score(word) == max_score }
-
-    if top_words.count == 1
-      best_word = top_words[0]
-    elsif check_seven_letter(top_words) != nil
-      best_word = check_seven_letter(top_words)
-    else
-      best_word = check_shortest_word(top_words)
-    end
-    return best_word
+    check_exceptions(top_words)
   end
-
 
   def self.check_seven_letter(array_of_words)
     sorted_by_length = array_of_words.sort_by { |word| word.length }
@@ -29,14 +21,22 @@ class Scrabble
     return seven_letter_words[0]
   end
 
+  def self.check_exceptions(top_words_array)
+    if top_words_array.count == 1
+      best_word = top_words_array[0]
+    elsif check_seven_letter(top_words_array) != nil
+      best_word = check_seven_letter(top_words_array)
+    else
+      best_word = check_shortest_word(top_words_array)
+    end
+  end
+
   def self.check_shortest_word(array_of_words)
-    sorted_by_length = array_of_words.sort_by { |word| word.length }
-    shortest_word = sorted_by_length[0]
-    return shortest_word
+    array_of_words.sort_by { |word| word.length }[0]
   end
 
   def self.letter_points(letter)
-    score = 0
+    letter_score = 0
 
     letter_hash = {["a","e", "i", "o", "u", "l", "n", "r", "s", "t"] => 1,
     ["d", "g"] => 2,
@@ -48,10 +48,10 @@ class Scrabble
 
     letter_hash.each {|key, value|
       if key.include? letter
-        score = value
+        letter_score = value
       end}
 
-    return score
+    return letter_score
   end
 
 end
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -1,36 +1,61 @@
 require './scrabble.rb'
 
 describe "Scrabble" do
-
   describe "#class methods here" do
 
-    it "calculates letter points" do
-      expect(Scrabble.letter_points("z")).to eq 10
+    context "#letter_points" do
+      it "calculates letter points" do
+        expect(Scrabble.letter_points("z")).to eq 10
+      end
+
+      it "calculates letter points as 0 when input is invalid" do
+        expect(Scrabble.letter_points(" ")).to eq 0
+        expect(Scrabble.letter_points("4")).to eq 0
+        expect(Scrabble.letter_points("!")).to eq 0
+        expect(Scrabble.letter_points("")).to eq 0
+      end
+
     end
 
-    it "gives correct score" do
-      expect(Scrabble.score("cat")).to eq 5
+    context "#score" do
+
+      it "gives correct score" do
+        expect(Scrabble.score("cat")).to eq 5
+      end
+
+      it "gives correct score when word is capitalized" do
+        expect(Scrabble.score("Cat")).to eq 5
+      end
+
+      it "gives score of 0 when input is invalid" do
+        expect(Scrabble.score("4739")).to eq 0
+        expect(Scrabble.score("48c9sajfsd")).to eq 0
+        expect(Scrabble.score("%1a ")).to eq 0
+
+      end
+
     end
 
-    it "gives correct score when word is capitalized" do
-      expect(Scrabble.score("CAT")).to eq 5
+    context "#highest_score_from" do
+
+      it "returns word with highest score from array of two" do
+        expect(Scrabble.highest_score_from(["cat", "queen"])).to eq "queen"
+      end
+
+      it "returns 7 letter word when there is a word score tie" do
+        expect(Scrabble.highest_score_from(["yokel", "ability", "zit"])).to eq "ability"
+      end
+
+      it "returns shorter word when there is a word score tie" do
+        expect(Scrabble.highest_score_from(["star", "dot"])).to eq "dot"
+      end
+
+      it "returns first word in list when scores and lengths match" do
+        expect(Scrabble.highest_score_from(["aaaaa", "eeeee", "iiiii"])).to eq "aaaaa"
+      end
     end
 
-    it "responds when score is called upon non-letters" do
-      expect(Scrabble.score("4739")).to eq 0
-    end
 
-    it "returns word with highest score" do
-      expect(Scrabble.highest_score_from(["cat", "queen"])).to eq "queen"
-    end
-
-    it "returns 7 letter word when there is a word score tie" do
-      expect(Scrabble.highest_score_from(["yokel", "ability", "zit"])).to eq "ability"
-    end
-
-    it "returns shorter word when there is a word score tie" do
-      expect(Scrabble.highest_score_from(["star", "dot"])).to eq "dot"
-    end
 
 
 
